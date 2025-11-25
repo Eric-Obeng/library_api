51b1a6cf4b9afcd8344d94661cba0b6a9a8734e0
Eric-Obeng <obengeric.3958@gmail.com>
2025-11-25 14:07:59 +0000

    feat: enhance book retrieval and management endpoints - Added HTTPException handling for invalid book IDs and not found cases. - Updated book data structure to use model attributes instead of dictionaries. - Implemented create and update book functionality with proper response models.

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from constant.book import Books
+from model.book import Book, CreateBook
 
 app = FastAPI()
 
-books = [
-    {
-        "id": 1,
-        "title": "The Pragmatic Programmer",
-        "author": "Hunt",
-        "category": "programming",
-    },
-    {"id": 2, "title": "Clean Code", "author": "Martin", "category": "programming"},
-    {"id": 3, "title": "1984", "author": "Orwell", "category": "fiction"},
-]
+books = Books
 
 
 # A. Root Endpoint
@@ -22,58 +15,92 @@ def root():
 
 # B. Get Book by ID
 @app.get("/books/{book_id}")
-def get_book_by_id(book_id: int):
+def get_book_by_id(book_id: int) -> Book:
     if book_id < 0:
-        return {"error": "Invalid book ID."}
+        raise HTTPException(status_code=400, detail="Invalid book ID.")
 
     for book in books:
-        if book["id"] == book_id:
+        if book.id == book_id:
             return book
 
-    return {"error": "Book not found."}
+    raise HTTPException(status_code=404, detail="Book not found.")
 
 
 # C. Search for Books
 @app.get("/search")
-def search_books(title: str = None, author: str = None, limit: int = 5):
+def search_books(title: str = None, author: str = None, limit: int = 5) -> dict:
     search_results = books
 
     if title:
         search_results = [
-            book for book in search_results if title.lower() in book["title"].lower()
+            book for book in search_results if title.lower() in book.title.lower()
         ]
 
     if author:
         search_results = [
-            book for book in search_results if author.lower() in book["author"].lower()
+            book for book in search_results if author.lower() in book.author.lower()
         ]
 
     search_results = search_results[:limit]
 
     if not search_results:
-        return {"results": [], "message": "No books found."}
+        raise HTTPException(status_code=400, detail="No books found.")
 
     return {"results": search_results, "count": len(search_results)}
 
 
 # D. Filter Books by Category
 @app.get("/category/{category_name}")
-def filter_book_by_category(category_name: str, sort: str = None):
+def filter_book_by_category(category_name: str, sort: str = None) -> dict:
     filtered_book = [
-        book for book in books if book["category"].lower() == category_name.lower()
+        book for book in books if book.category.lower() == category_name.lower()
     ]
 
     if sort:
         sort_lower = sort.lower()
         if sort_lower not in ("asc", "desc"):
-            return {"error": "Invalid sort value. User 'asc' or 'desc'."}
+            raise HTTPException(
+                status_code=400, detail="Invalid sort value. User 'asc' or 'desc'."
+            )
         reverse = sort_lower == "desc"
-        filtered_book.sort(key=lambda b: b["title"].lower(), reverse=reverse)
+        filtered_book.sort(key=lambda b: b.title.lower(), reverse=reverse)
 
     if not filtered_book:
-        return {"results": [], "message": "No books found."}
+        raise HTTPException(status_code=400, detail="No books found.")
 
     return {"result": filtered_book, "count": len(filtered_book)}
+
+
+@app.post("/book", response_model=Book, status_code=201)
+def create_book(payload: CreateBook):
+    _id = len(books) + 1
+
+    new_book = Book(
+        id=_id,
+        title=payload.title,
+        author=payload.author,
+        category=payload.category,
+        description=payload.description,
+    )
+    books.append(new_book)
+
+    return new_book
+
+
+@app.put("/books/{book_id}")
+def update_book(book_id: int, payload: CreateBook) -> Book:
+    if book_id < 0 or book_id > len(books):
+        raise HTTPException(status_code=400, detail="Invalid book ID.")
+
+    index = book_id - 1
+
+    book = books[index]
+    updated_book = payload.model_dump()
+
+    for field, value in updated_book.items():
+        setattr(book, field, value)
+
+    return book
 
 
 # uvicorn start
